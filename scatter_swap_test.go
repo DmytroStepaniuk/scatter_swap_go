9cc17db7a2ac67b3da31219086c2b5b7934a7a91
Dmytro Stepaniuk <stepaniuk@mlsdev.com>
2021-11-04 13:23:02 +0200

    Update: test

--- scatter_swap_test.go
+++ scatter_swap_test.go
@@ -7,12 +7,25 @@ import (
 func TestAverage(t *testing.T) {
 	hashService := Service{Spin: 1}
 
-  for i := 0; i <= 9_999_999_999; i++ {
-		result := hashService.HashToString(i)
+	digits := []int{
+		9,
+		90,
+		900,
+		9000,
+		90000,
+		900000,
+		9000000,
+		90000000,
+		900000000,
+		9999999999,
+	}
+
+  for _, digit := range digits {
+		result := hashService.HashToString(digit)
 
 		if len(result) != 10 {
 			t.Error(
-				"For", i,
+				"For", digit,
 				"expected", "string that contains 10 digits",
 				"got", result,
 			)
